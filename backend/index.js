44873e41503ea6b3b06e66633e624e5e1e726c01
Param <parambansal97@gmail.com>
2025-09-14 06:38:56 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: 'http://localhost:8000/',
+    origin: 'https://connect-agub.onrender.com',
     credentials: true
 }
 app.use(cors(corsOptions));
